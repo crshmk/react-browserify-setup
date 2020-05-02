fc434e4d40151d20152ddd725fda0d76d7aed9fe
Chris Hammock <c_hammock@apple.com>
2020-05-01 22:39:49 -0500

    css reset and fonts

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
 export default function Home() {
-  return <p>home page</p>
+  return (
+    <div>
+      <h1>Biggest heading</h1>
+      <h2>less big heading</h2>
+      <h3>small heading</h3>
+      <p>regular old text</p>
+    </div>
+  )
 }
